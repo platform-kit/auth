3e7c23eeb1b8aa6ccc4e3c34881ac395a5bfdeca
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2022-07-25 02:59:28 -0700

    Update app.js

--- app.js
+++ app.js
@@ -82,6 +82,7 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   //res.render("error");
+  console.log(err);
   res.redirect("/error");
 });
 
